1983007e75a4054c2a4c6c0439a556c2835b2002
samson <malonee20@gmail.com>
2020-01-28 19:12:08 +0200

    go commit

--- src/CardList.js
+++ src/CardList.js
@@ -1,11 +1,31 @@
 import React from 'react';
-
-const CardList = () => {
-  return (
+import Card from './Card';
+const CardList = ({robots}) => {
+    /*const cardArray = robots.map((user, i)=>{
+     return (
+     <Card 
+    key={user.id}
+     id={robots[i].id} 
+     name={robots[i].name} 
+     email={robots[i].email}
+      /> 
+     );
+    });*/
+    return (
     <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[0].email} />
+    {
+      /* {cardArray} */
+        robots.map((user, i)=>{
+     return (
+     <Card 
+    key={user.id}
+     id={robots[i].id} 
+     name={robots[i].name} 
+     email={robots[i].email}
+      /> 
+     );
+    })    
+   }
     </div>
   );
 }
