230d0295202936b401cf9c46ae18c147a1991a50
samson <malonee20@gmail.com>
2020-02-01 08:31:30 +0200

    today commmit

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,9 +3,9 @@ import React from 'react'
 const SearchBox = ({searchChange}) => {
   return (
     <div className='pa2'>
-  <input className='pa3 ba b--green bg-lightest-blue'
+  <input className='w5 pa3 ba b--blue bg-lightest-green'
    type = 'search'
-   placeholder= 'search robots' 
+   placeholder= 'search robot friends' 
      onChange={searchChange}
    />
 
