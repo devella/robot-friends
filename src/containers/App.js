f9947545d5b5bc064b309e92298ef18c23620e10
samson <malonee20@gmail.com>
2020-02-01 21:18:24 +0200

    update commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import SearchBox from '.-/components/SearchBox';
+import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import'./containers/App';
+import'./App';
 
 
 class App extends Component {
