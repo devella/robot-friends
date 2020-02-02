28b03f7d45c4d2ae2faf8b830d4980295eb535e7
samson <malonee20@gmail.com>
2020-02-02 20:06:01 +0200

    Updates

---
+++ precache-manifest.18aef6de888807c944df2722897396fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ef07b415688c7d51fdcf0464f3acc5",
+    "url": "/robot-friends/index.html"
+  },
+  {
+    "revision": "5ebb63fda25d5550e9cd",
+    "url": "/robot-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "97da6bef497393efc35a",
+    "url": "/robot-friends/static/css/main.15fe9b6c.chunk.css"
+  },
+  {
+    "revision": "5ebb63fda25d5550e9cd",
+    "url": "/robot-friends/static/js/2.ee9db53a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robot-friends/static/js/2.ee9db53a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97da6bef497393efc35a",
+    "url": "/robot-friends/static/js/main.77c94c22.chunk.js"
+  },
+  {
+    "revision": "71e1c4b367bb7207f55c",
+    "url": "/robot-friends/static/js/runtime-main.57950e2e.js"
+  },
+  {
+    "revision": "cb5b7b5713ea925d3af881d208607b0b",
+    "url": "/robot-friends/static/media/Comic Queens Italic.cb5b7b57.woff"
+  }
+]);
