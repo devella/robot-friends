bf8bcc3749c33a91178c462519328ced39c6b9be
Samson <malonee20@gmail.com>
2020-04-07 22:41:56 +0300

    checked

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,17 +5,13 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import'./App.css';
-
 import { setSearchField, requestRobots } from '../actions';
-
 
 const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
     isPending: state.requestRobots.isPending,
-    
-    
   }
 }
 
@@ -27,15 +23,9 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-
-  
-  
   componentDidMount() {
     this.props.onRequestRobots();
   }
-
-
-  
 render() {
     const { robots, searchField, onSearchChange, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
